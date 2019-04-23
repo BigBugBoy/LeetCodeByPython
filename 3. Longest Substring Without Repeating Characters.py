1d7f63307aea1fd8fe5c5ea9021316743218a5de
bigbugboy <ash118wwj@126.com>
2019-04-23 19:49:04 +0800

    update annotate

--- 3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters.py
@@ -18,8 +18,8 @@ class Solution:
         ls = []
         ls.append(s[0])
         self.result = len(ls)
-        maxLen = 1
-        maxLs = []
+        maxLen = 1 # 从1开始，前边已经判断过，排除了空列表情况，列表至少有一个
+        # maxLs = []
         for i in range(le-1):
             if(s[i+1] not in ls):
                 # 下一个字符不在结果队列ls里边，那么进行相关增加操作
@@ -27,15 +27,15 @@ class Solution:
                 ls.append(s[i+1])
                 if maxLen <= self.result:
                     maxLen = self.result
-                    maxLs = ls[:]
+                    # maxLs = ls[:]
             else:
                 # 此处有两种情况，最后一个字符与当前字符相同，最后一个字符与当前字符不同
                 p = ls.index(s[i+1])
                 ls.append(s[i+1])
                 del ls[:p+1]
-                maxLs = ls[:]
+                # maxLs = ls[:]
                 self.result = len(ls)
-        return maxLen, maxLs
+        return maxLen
 
 if __name__ == "__main__":
     testAnswer = Solution('abcba')
