2fa100b2e9e0b18c95f45360021ff5a67ae4d8f7
bigbugboy <ash118wwj@126.com>
2019-05-31 22:51:48 +0800

    测试

--- 39. Combination Sum.py
+++ 39. Combination Sum.py
@@ -1,6 +1,20 @@
 # -*- coding:UTF-8 -*-
 # Author: bigbugboy
 # Software: VS Code
-# Datatime: 2019.4.3
+# Datatime: 2019.5.5
 
-TODO
+
+import math
+
+class Solution:
+    def combinationSum(self, candidates, target):
+        resultList = []
+        sorted(candidates) # 对列表进行排序，默认升序
+        
+        return resultList
+
+    def findResultList(cl, target, rl):
+        # 找到target所在的位置，记为cursor
+        for r in range(target):
+
+
